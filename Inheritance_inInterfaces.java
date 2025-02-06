4a5034a35df392665b5ed4c75d5a29628689804d
Rohit Kamble <rohitkamble171012@gmail.com>
2025-02-06 19:59:25 +0530

    Add files via upload

---
+++ Inheritance_inInterfaces.java
@@ -0,0 +1,33 @@
+package com.company;
+
+
+interface sampleInterface{
+    void meth1();
+    void meth2();
+}
+interface childSampleInterface extends sampleInterface{
+    void meth3();
+    void meth4();
+}
+class MySampleClass implements childSampleInterface{
+    public void meth1(){
+        System.out.println("meth1");
+    }
+    public void meth2(){
+        System.out.println("meth2");
+    }
+    public void meth3(){
+        System.out.println("meth3");
+    }
+    public void meth4(){
+        System.out.println("meth4");
+    }
+
+}
+public class Inheritance_inInterfaces {
+    public static void main(String[] args) {
+        MySampleClass mc = new MySampleClass();
+        mc.meth1();
+
+    }
+}
